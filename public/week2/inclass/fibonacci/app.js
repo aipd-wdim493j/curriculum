daed629a51eed1327b7ce2ad123ff13f53591cd3
mario lucero <Mario12684@yahoo.com>
2012-08-02 21:27:47 -0700

    Node class work WK4

---
+++ public/week2/inclass/fibonacci/app.js
@@ -0,0 +1,9 @@
+var fib = require('child process'); 
+
+	setTimeout(function() {
+		colsole.log("setTimeout");
+}, 1000);
+
+console.time("fib");
+fib(41);
+console.timeEnd("fib");
--- public/week2/inclass/scraper/scraper.js
+++ public/week2/inclass/scraper/scraper.js
@@ -1,9 +1,16 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var argv = require('optimist').argv;
+var request = require('request'),
+       cheerio = require('cheerio'),
+       argv = require('optimist').argv;
 
+	   
+	   
+console.log(process.argv);
+console.log("starting to fetch ALA number." );
+console.time("cogsworth");
 request('http://www.alistapart.com', function (error, response, body) {
   if (!error && response.statusCode == 200) {
+	console.log("finished the fetch.");
+	console.timeEnd("cogsworth");
     $ = cheerio.load(body);
     console.log($("#ish em").text());
   }
